27a4d1a8cf144968979e99c459c76b80285e3aeb
Jongun Jeong <skeep194@knu.ac.kr>
2021-06-29 15:25:10 +0900

    add artist entity

---
+++ src/common/entity/artist.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Music } from './music.entity';
+
+@Entity()
+export class Artist {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @ManyToMany(() => Music, music => music.artists)
+  musics: Music[];
+}
--- src/common/entity/music.entity.ts
+++ src/common/entity/music.entity.ts
@@ -5,10 +5,36 @@ import {
   OneToMany,
   ManyToMany,
 } from 'typeorm';
+import { Artist } from './artist.entity';
 import { MusicComment } from './music-comment.entity';
 import { MusicLike } from './music-like.entity';
 import { MusicTag } from './music-tag.entity';
 import { User } from './user.entity';
+
+//TODO: fill enum value
+export enum VocalType {
+
+}
+
+export enum Language {
+
+}
+
+export enum RhythmBeat {
+
+}
+
+export enum BPM {
+
+}
+
+export enum MelodyScale {
+
+}
+
+export enum Copyright {
+
+}
 
 @Entity()
 export class Music {
@@ -24,6 +50,33 @@ export class Music {
   @Column()
   lyricist: string;
 
+  @Column({type: 'text'})
+  lyrics: string;
+
+  @Column({type: 'date'})
+  dates: Date;
+
+  @Column()
+  album: string;
+
+  @Column({type: 'enum', enum: VocalType})
+  vocalType: VocalType;
+
+  @Column({type: 'enum', enum: Language})
+  language: Language;
+
+  @Column({type: 'enum', enum: RhythmBeat})
+  rhythmBeat: RhythmBeat;
+
+  @Column({type: 'enum', enum: BPM})
+  bpm: BPM;
+
+  @Column({type: 'enum', enum: MelodyScale})
+  melodyScale: MelodyScale;
+
+  @Column({type: 'enum', enum: Copyright})
+  copyright: Copyright;
+
   @Column()
   link: string;
 
@@ -37,4 +90,7 @@ export class Music {
 
   @OneToMany(() => MusicTag, (musicTag) => musicTag.music, { cascade: true })
   musicTags: MusicTag[];
+
+  @ManyToMany(() => Artist, artist => artist.musics)
+  artists: Artist[];
 }
