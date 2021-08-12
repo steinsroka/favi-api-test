189ce2c583aae091d7573ba37ae75c4d7621c7f6
Kadrick <kbk2581553@gmail.com>
2021-08-12 17:38:15 +0900

    global pipe options

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,14 @@ async function bootstrap() {
   }
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
   );
   app.use(cors());
   app.use(helmet());
