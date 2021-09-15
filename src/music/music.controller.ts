4a0ee8d4ec8aa36830d734c06c19393268799842
c1000ho0 <c1000ho0@gmail.com>
2021-09-15 17:17:56 +0900

    test social

--- src/music/music.controller.ts
+++ src/music/music.controller.ts
@@ -62,7 +62,7 @@ export class MusicController {
   async getMusicWithArtist(
     // @Request() req: UserRequest,
     @Param('id') id: number,
-  ): Promise<Artist> {
+  ): Promise<Music[]> {
     const music = await this.musicService.getMusicWithArtist(id);
     return music;
   }
