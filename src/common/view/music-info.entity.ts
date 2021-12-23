cb3c8a87e61c95b048d5b3d79d1dab8f0968719b
c1000ho0 <c1000ho0@gmail.com>
2021-12-24 02:03:14 +0900

    1224 comment count

--- src/common/view/music-info.entity.ts
+++ src/common/view/music-info.entity.ts
@@ -35,7 +35,7 @@ import { MusicTagInfo } from './music-tag-info.entity';
       .addSelect('music.melodyScale', 'melodyScale')
       .addSelect('music.copyright', 'copyright')
       .addSelect('COUNT(musicLike.userId)', 'likedUserCount')
-      .addSelect('COUNT(musicComment.userId)', 'commentedCount')
+      .addSelect('COUNT(musicComment.id)', 'commentedCount')
       .from(Music, 'music')
       .leftJoin('music.musicLikes', 'musicLike')
       .leftJoin('music.musicComments', 'musicComment')
