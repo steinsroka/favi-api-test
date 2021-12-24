6f68119f6b3da1696b6ad00438d0cbc42badaa39
c1000ho0 <c1000ho0@gmail.com>
2021-12-24 13:54:25 +0900

    1224 user follow

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -7,6 +7,7 @@ import { Album } from '../common/entity/album.entity';
 import { JwtStrategy } from '../auth/strategy/jwt.strategy';
 import { UserInfo } from '../common/view/user-info.entity';
 import { UserTagInfo } from '../common/view/user-tag-info.entity';
+import { UserFollow } from '../common/entity/user-follow.entity';
 import { MusicTagInfo } from '../common/view/music-tag-info.entity';
 import { MusicLike } from '../common/entity/music-like.entity';
 import { Music } from '../common/entity/music.entity';
@@ -22,6 +23,7 @@ import { AuthModule } from '../auth/auth.module';
       Album,
       UserInfo,
       UserTagInfo,
+      UserFollow,
       MusicLike,
       Music,
       SocialLog,
