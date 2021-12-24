e80949becbd77744d6327071a18518cd02549907
c1000ho0 <c1000ho0@gmail.com>
2021-12-24 13:30:22 +0900

    1224 beat comment

--- src/beat/beat.module.ts
+++ src/beat/beat.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Beat } from '../common/entity/beat.entity';
 import { BeatLike } from '../common/entity/beat-like.entity';
+import { BeatComment } from '../common/entity/beat-comment.entity';
 import { BeatController } from './beat.controller';
 import { BeatService } from './beat.service';
 import { BeatInfo } from '../common/view/beat-info.entity';
