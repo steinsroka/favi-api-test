2e8d4271cef30a06f38477aa3a388dbcfdd3c6e9
c1000ho0 <c1000ho0@gmail.com>
2021-12-26 03:33:56 +0900

    1224 search beat

--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MusicModule } from '../music/music.module';
+import { BeatModule } from '../beat/beat.module';
 import { SearchController } from './search.controller';
 import { SearchService } from './search.service';
 
 @Module({
-  imports: [MusicModule],
+  imports: [MusicModule,BeatModule],
   controllers: [SearchController],
   providers: [SearchService],
 })
