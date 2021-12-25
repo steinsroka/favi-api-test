8f02eddd4ae94aff73762ce0f2cbacdbfdd24d58
c1000ho0 <c1000ho0@gmail.com>
2021-12-26 05:47:55 +0900

    1226 music comment

--- src/common/view/music-info.entity.ts
+++ src/common/view/music-info.entity.ts
@@ -35,12 +35,12 @@ import { MusicTagInfo } from './music-tag-info.entity';
       .addSelect('music.melodyScale', 'melodyScale')
       .addSelect('music.copyright', 'copyright')
       .addSelect('COUNT(musicLike.userId)', 'likedUserCount')
-      .addSelect('COUNT(musicComment.comment)', 'commentedCount')
+      // .addSelect('COUNT(musicComment.comment)', 'commentedCount')
       .from(Music, 'music')
       .leftJoin('music.musicLikes', 'musicLike')
       .leftJoin('music.musicComments', 'musicComment')
+      .loadRelationCountAndMap('music.commentedCount', 'music.musicComments')
       .groupBy('music.id'),
-      .addGroupBy('musicComment.musicId')
 })
 export class MusicInfo {
   @ViewColumn()
